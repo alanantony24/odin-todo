597ca3700e7bea43cc4c4a965cb99f9896e24691
alanantony24 <s10207327@connect.np.edu.sg>
2023-10-14 00:14:31 +0800

    added a sidebar structure

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log("Initiate webpack...");
+import './style.css';
+
+const hamburgerBtn = document.querySelector(".hamburgerBtn");
+hamburgerBtn.addEventListener('click', () => {
+    const sideBar = document.querySelector(".sideBar");
+    sideBar.classList.toggle("hidden");
+})
