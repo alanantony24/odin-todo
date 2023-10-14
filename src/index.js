c5448de046d772da625ce0ad6993b3c97e9fcdb4
alanantony24 <s10207327@connect.np.edu.sg>
2023-10-14 16:05:07 +0800

    switching lightmode and darkmode implemented

--- src/index.js
+++ src/index.js
@@ -5,3 +5,16 @@ hamburgerBtn.addEventListener('click', () => {
     const sideBar = document.querySelector(".sideBar");
     sideBar.classList.toggle("hidden");
 })
+
+const toggleLightMode = document.getElementById("darkmodeToggle");
+if (toggleLightMode.checked === true) {
+    document.body.classList.add("light");
+}
+
+else {
+    document.body.classList.remove("light");
+}
+
+toggleLightMode.addEventListener("change", () => {
+    document.body.classList.toggle("light");
+});
