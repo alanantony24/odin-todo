5cb632c22cafd857c0a978c57a7bc6f4088a5fd9
alanantony24 <s10207327@connect.np.edu.sg>
2023-10-15 19:48:47 +0800

    create base classes and getter/setter

--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
 import './style.css';
 
+//to toggle the sidebar.
 const hamburgerBtn = document.querySelector(".hamburgerBtn");
 hamburgerBtn.addEventListener('click', () => {
     const sideBar = document.querySelector(".sideBar");
     sideBar.classList.toggle("hidden");
 })
 
+//check what's the theme at the start of the app.
 const toggleLightMode = document.getElementById("darkmodeToggle");
 if (toggleLightMode.checked === true) {
-    document.body.classList.add("light");
+    document.body.classList.add("dark");
 }
 
 else {
-    document.body.classList.remove("light");
+    document.body.classList.remove("dark");
 }
 
+//when the switch is checked, toggle between light and dark modes.
 toggleLightMode.addEventListener("change", () => {
-    document.body.classList.toggle("light");
+    document.body.classList.toggle("dark");
 });
---
+++ src/modules/tasks.js
@@ -0,0 +1,41 @@
+class Task {
+    constructor(title, description, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+
+    setTitle(title) {
+        this.title = title;
+    }
+
+    getTitle() {
+        return this.title;
+    }
+
+    setDescription(description) {
+        this.description = description;
+    }
+
+    getDescription() {
+        return this.description;
+    }
+
+    setDueDate(dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    getDueDate() {
+        return this.dueDate;
+    }
+
+    setPriority(priority) {
+        this.priority = priority;
+    }
+
+    getTitle() {
+        return this.priority;
+    }
+    
+}
